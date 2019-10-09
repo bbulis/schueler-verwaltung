85fc1e24619be867623341f20c85749304777c9a
Benjamin Bulis <bbulis@student.tgm.ac.at>
2019-10-09 14:21:26 +0200

    "Backend" finished Working on "Frontend" Working with EJS

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Sequelize = require('sequelize');
 // Import der Umgebungsvariablen
 require('dotenv').config();
 
+// TODO Implementing EJS for Rendering
+
 // Erstellen von einem ExpressJS Server
 const app = express();
 
@@ -22,19 +24,10 @@ const sequelize = new Sequelize(process.env.DB_DATABASE,
                                 }
     );
 
-// Testen der Datenbankverbindung
-app.get('/db', (req, res) => {
-    sequelize.authenticate().then(() => {
-        console.log('DB Works')
-    }).catch((err) => {
-        console.log('Error: ' + err.toString())
-    });
-    res.send();
-});
-
 /**
  * Erstellen eines Schueler Objektes
  * 4 Felder ( vorname, nachname, klasse, ausbildungszweig )
+ * Das erste Argument bei sequelize.define ist der Table-Name. So wird dieser Später in der Datenbank lauten
  */
 const User = sequelize.define('schueler', {
     vorname: {
@@ -55,15 +48,48 @@ const User = sequelize.define('schueler', {
     }
 });
 
+// Testen der Datenbankverbindung
+app.get('/db', (req, res) => {
+    sequelize.authenticate().then(() => {
+        console.log('DB Works')
+    }).catch((err) => {
+        console.log('Error: ' + err.toString())
+    });
+    res.send();
+});
+
+/**
+ * Wird dieser Endpoint angefragt so wird der Table gelöscht und neu gesetzt.
+ * force: true - Table löschen und neu setzten
+ * force: false - Table neu setzten. Gelöscht wird nicht. Kann zu Errors führen
+ */
+app.get('/db/destroy', async (req, res) => {
+     try {
+         await User.sync({force: true})
+     } catch (e) {
+         // TODO Render Error when deleting Table
+     }
+});
+
 /**
  * Home route
  * Eintrittspunkt wenn man auf die Seite kommt. Dies wird am Client angezeigt
  */
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+
+    try {
+        let schueler = await User.findAll();
+    } catch (e) {
+        // TODO Render Error when fetching schueler
+    }
+
+    // TODO Render Table with schueler
     // TODO Render Home Page
 });
 
-// Erstellungs route für einen neuen Schueler
+/**
+ * Erstellungs Route für einen neuen Schueler
+ */
 app.get('/schueler/create', async (req, res) => {
     // Aus dem Request Objekt die notwendigen Informationen herausholen
     let vorname = req.body.vorname;
@@ -74,9 +100,36 @@ app.get('/schueler/create', async (req, res) => {
      * Pürfen ob ein Schueler schon vorhanden ist
      * Ist dieser Vorhanden so wird der created auf False gesetzt und kein User zurückgegeben
      */
-    let [user, created] = await User.findOrCreate({where: {vorname: vorname, nachname: nachname}, defaults: {klasse: klasse, ausbildungszweig: ausbildungszweig}})
+    try {
+        let [user, created] = await User.findOrCreate({where: {vorname: vorname, nachname: nachname}, defaults: {klasse: klasse, ausbildungszweig: ausbildungszweig}})
+    } catch (e) {
+        // TODO Render Error for creating
+    }
 
     // TODO return Statment for Validating if Schueler existst and rendering File
+});
+
+/**
+ * Endpoint zum löschen eines Schuelers
+ */
+app.delete('/schueler/delete', async (req, res) => {
+    let vorname = req.body.vorname;
+    let nachname = req.body.nachname;
+    let klasse = req.body.klasse;
+
+    try {
+        await User.destroy({
+            where: {
+                vorname: vorname,
+                nachname: nachname,
+                klasse: klasse
+            }
+        })
+    } catch (e) {
+        // TODO Render Error for deleting
+    }
+
+    // TODO Render for Deleting Schueler
 });
 
 // Setzen des Ports für den ExpressJS Server
